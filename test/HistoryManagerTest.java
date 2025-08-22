98ad4f59484c48e9c5b8c768d91b53802b3cd576
Test <test@example.com>
2025-08-23 01:33:33 +0300

    Add HistoryManagerTest

---
+++ test/HistoryManagerTest.java
@@ -0,0 +1,28 @@
+import manager.InMemoryHistoryManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import task.Task;
+import task.TaskStatus;
+
+public class HistoryManagerTest {
+    InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+    @Test
+    void checkTaskOldVersionSaved() {
+        Task task = new Task(
+                "Сделать зарядку",
+                "Пробежать 30 минут",
+                1,
+                TaskStatus.NEW
+        );
+        historyManager.add(task);
+        task = new Task(
+                "Испечь пирог",
+                "-",
+                2,
+                TaskStatus.NEW
+        );
+        Assertions.assertNotEquals(historyManager.getHistory().getFirst(), task);
+    }
+}
