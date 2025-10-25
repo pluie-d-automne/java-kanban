c2c9d286774aeaeba36e39571c9ddb6d5d3a85ab
Test <test@example.com>
2025-10-25 15:37:08 +0300

    Add methods to BaseHttpHandler

--- src/api/BaseHttpHandler.java
+++ src/api/BaseHttpHandler.java
@@ -1,7 +1,35 @@
 package api;
 
-import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class BaseHttpHandler {
+    protected void sendText(HttpExchange httpExchange, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        httpExchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        httpExchange.sendResponseHeaders(200, resp.length);
+        httpExchange.getResponseBody().write(resp);
+        httpExchange.close();
+    }
 
+    protected void sendNotFound(HttpExchange httpExchange) throws IOException {
+        String text = "Запрашиваемый объект не найден";
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        httpExchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+        httpExchange.sendResponseHeaders(404, resp.length);
+        httpExchange.getResponseBody().write(resp);
+        httpExchange.close();
+    }
+
+    protected void sendHasOverlaps(HttpExchange httpExchange) throws IOException {
+        String text = "Задача пересекается по времени с другими задачами.\n" +
+                      "Укажите другое время начала и/или продолжительность";
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        httpExchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+        httpExchange.sendResponseHeaders(400, resp.length);
+        httpExchange.getResponseBody().write(resp);
+        httpExchange.close();
+    }
 }
